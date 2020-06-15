b4b52416c146dbce23dddc66db0478aed6faccac
Dima Tsoy <dimktsoy@yahoo.com>
2020-06-15 09:55:53 +0600

    Add range slider

---
+++ src/components/range-slider/range-slider.js
@@ -0,0 +1,32 @@
+import noUiSlider from 'nouislider';
+
+export default function rangeSlider() {
+  const $rangeSlider = $('#range-slider');
+  const control = $rangeSlider.find('.range-slider__control').get(0);
+  const value = $rangeSlider.find('.range-slider__value').get(0);
+
+  console.log($rangeSlider);
+
+  noUiSlider.create(control, {
+    start: [5000, 10000],
+    connect: true,
+    step: 100,
+    range: {
+      'min': [0],
+      'max': [16000],
+    },
+    format: {
+      to: function (value) {
+        return parseInt(value) + '₽';
+      },
+      from: function (value) {
+        return parseInt(value);
+      }
+    },
+    cssPrefix: 'range-slider__'
+  });
+
+  control.noUiSlider.on('update', values => {
+    value.innerHTML = values.map((num) => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')).join(' - ');
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './scss/main.scss';
+
+import rangeSlider from './components/range-slider/range-slider';
+rangeSlider()
