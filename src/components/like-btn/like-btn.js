8818af64335d3c6dc88fee825b4d8e238e28bc84
Dima Tsoy <dimktsoy@yahoo.com>
2020-06-22 21:30:15 +0600

    Update like-btn

--- src/components/like-btn/like-btn.js
+++ src/components/like-btn/like-btn.js
@@ -1,1 +1,25 @@
 import './like-btn.scss';
+
+const $likeBtn = $('.js-like-btn');
+
+function handleClick(e) {
+  const $count = $(this).find('.like-btn__count');
+  const isActive = $(this).hasClass('like-btn--active');
+  let countValue = parseInt($count.text(), 10);
+
+  if (countValue === 0) {
+    return false;
+  }
+
+  if (isActive) {
+    countValue -= 1;
+  } else {
+    countValue += 1;
+  }
+
+  $(this).toggleClass('like-btn--active');
+
+  $count.text(countValue);
+}
+
+$likeBtn.on('click', handleClick);
