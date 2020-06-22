3310b677e449beb4d30502e7b2bf8dee7139fd63
Dima Tsoy <dimktsoy@yahoo.com>
2020-06-22 23:57:06 +0600

    Update range-slider

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -1,29 +1,31 @@
 import noUiSlider from 'nouislider';
+
+import 'nouislider/distribute/nouislider.css';
 import './range-slider.scss';
 
-const $rangeSlider = $('#range-slider');
-const control = $rangeSlider.find('.range-slider__control').get(0);
-const value = $rangeSlider.find('.range-slider__value').get(0);
+$('.js-range-slider').each(function() {
+  const control = $(this).find('.range-slider__control').get(0);
+  const value = $(this).find('.range-slider__value').get(0);
 
-noUiSlider.create(control, {
-  start: [5000, 10000],
-  connect: true,
-  step: 100,
-  range: {
-    'min': [0],
-    'max': [16000],
-  },
-  format: {
-    to: function (value) {
-      return parseInt(value) + '₽';
+  noUiSlider.create(control, {
+    start: [5000, 10000],
+    connect: true,
+    step: 100,
+    range: {
+      'min': [0],
+      'max': [16000],
     },
-    from: function (value) {
-      return parseInt(value);
+    format: {
+      to: function (value) {
+        return parseInt(value) + '₽';
+      },
+      from: function (value) {
+        return parseInt(value);
+      }
     }
-  },
-  cssPrefix: 'range-slider__'
-});
-
-control.noUiSlider.on('update', values => {
-  value.innerHTML = values.map((num) => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')).join(' - ');
+  });
+  
+  control.noUiSlider.on('update', values => {
+    value.innerHTML = values.map((num) => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')).join(' - ');
+  });
 });
