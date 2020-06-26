d18603052762ce3dc2509b88c914594e66e0b215
Dima Tsoy <dimktsoy@yahoo.com>
2020-06-26 12:39:51 +0600

    Refactor date-dropdown

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -1,5 +1,5 @@
-import 'air-datepicker';
 import $ from 'jquery';
+import 'air-datepicker';
 
 import '../../../node_modules/air-datepicker/src/sass/datepicker.scss';
 import '../../../node_modules/air-datepicker/src/sass/cell.scss';
@@ -7,59 +7,79 @@ import '../../../node_modules/air-datepicker/src/sass/navigation.scss';
 import '../../../node_modules/air-datepicker/src/sass/timepicker.scss';
 import './date-dropdown.scss';
 
-const config = {
-  clearButton: 'true',
-  range: 'true',
-  minDate: new Date(),
-  multipleDatesSeparator: ' - ',
-  navTitles: {
-    days: 'MM yyyy'
-  },
-  position: 'bottom left',
-  prevHtml:
-    '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF" /></svg >',
-  nextHtml:
-    '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.36301 0.984375L16.3786 9L8.36301 17.0156L6.95676 15.6094L12.5349 9.98438H0.347383V8.01562H12.5349L6.95676 2.39062L8.36301 0.984375Z" fill="#BC9CFF" /></svg>',
-  onShow(inst, animationCompleted) {
-    if (!animationCompleted) {
-      if (!inst.$datepicker.find('.datepicker--buttons .datepicker--button-apply').length) {
-        $('<span class="datepicker--button datepicker--button-apply">Применить</span>')
-          .on('click', function handleClick(e) {
-            e.stopPropagation();
-            inst.hide();
-          })
-          .appendTo(inst.$datepicker.find('.datepicker--buttons'));
-      }
+class DateDropdown {
+  constructor($component) {
+    this.$component = $component;
+    this.isFilter = !!$component.find('.js-date-dropdown__input-filter').length;
+    this.init();
+  }
+
+  init() {
+    if (this.isFilter) {
+      this.datepicker = this.$component
+        .find('.js-date-dropdown__input-filter')
+        .datepicker({ ...DateDropdown.getDatepickerConfig(), dateFormat: 'd M' })
+        .data('datepicker');
+    } else {
+      const $inputStart = this.$component.find('.js-date-dropdown__input-start');
+      const $inputEnd = this.$component.find('.js-date-dropdown__input-end');
+
+      this.datepicker = $inputStart
+        .datepicker({
+          ...DateDropdown.getDatepickerConfig(),
+          minDate: new Date(),
+          onSelect(formattedDate) {
+            $inputStart.val(formattedDate.split(' - ')[0]);
+            $inputEnd.val(formattedDate.split('-')[1]);
+          }
+        })
+        .data('datepicker');
+
+      $inputEnd.on('click', DateDropdown.handleInputEndClick);
     }
   }
-};
 
-const $dateDropdown = $('.js-date-dropdown');
-const $inputFilter = $dateDropdown.find('.js-date-dropdown__input-filter');
-const $inputStart = $dateDropdown.find('.js-date-dropdown__input-start');
-const $inputEnd = $dateDropdown.find('.js-date-dropdown__input-end');
+  static handleInputEndClick(event) {
+    $(event.currentTarget)
+      .closest('.js-date-dropdown')
+      .find('.js-date-dropdown__input-start')
+      .focus();
+  }
 
-function dateDropdownInit() {
-  $inputStart.each(function init() {
-    const $this = $(this);
-    $this.datepicker({
-      ...config,
-      onSelect(fd) {
-        $this.val(fd.split(' - ')[0]);
-        $this.closest('.date-dropdown').find('.js-date-dropdown__input-end').val(fd.split('-')[1]);
+  static handleApplyButtonClick(event) {
+    event.stopPropagation();
+    this.hide();
+  }
+
+  static getDatepickerConfig() {
+    return {
+      clearButton: true,
+      range: true,
+      multipleDatesSeparator: ' - ',
+      dateFormat: 'dd.mm.yyyy',
+      navTitles: {
+        days: 'MM yyyy'
+      },
+      prevHtml:
+        '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"/></svg >',
+      nextHtml:
+        '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.36301 0.984375L16.3786 9L8.36301 17.0156L6.95676 15.6094L12.5349 9.98438H0.347383V8.01562H12.5349L6.95676 2.39062L8.36301 0.984375Z" fill="#BC9CFF"/></svg>',
+      onShow(inst, animationCompleted) {
+        if (!animationCompleted) {
+          if (!inst.$datepicker.find('.datepicker--button-apply').length) {
+            $('<span class="datepicker--button datepicker--button-apply">Применить</span>')
+              .on('click', DateDropdown.handleApplyButtonClick.bind(inst))
+              .appendTo(inst.$datepicker.find('.datepicker--buttons'));
+          }
+        }
       }
-    });
-  });
-  $inputEnd.on('click', function handleClick() {
-    $(this).closest('.date-dropdown').find('.js-date-dropdown__input-start').focus();
-  });
+    };
+  }
 }
 
-function dateDropdownFilterInit() {
-  $inputFilter.each(function init() {
-    $(this).datepicker({ ...config, dateFormat: 'dd M' });
+$(() => {
+  $('.js-date-dropdown').each((index, node) => {
+    const dateDropdown = new DateDropdown($(node));
+    return dateDropdown;
   });
-}
-
-dateDropdownInit();
-dateDropdownFilterInit();
+});
