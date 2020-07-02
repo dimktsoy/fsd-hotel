f5c7abb6b15afe96f2623b7d9c12de0e64051873
Dima Tsoy <dimktsoy@yahoo.com>
2020-07-02 23:28:14 +0600

    Update field-text

--- src/components/field-text/field-text.js
+++ src/components/field-text/field-text.js
@@ -1,1 +1,11 @@
+import Inputmask from "inputmask";
+import $ from 'jquery';
+
 import './field-text.scss';
+
+$(() => {
+  $('.js-field-text-mask').each((index, node) => {
+    const im = new Inputmask();
+    im.mask(node);
+  });
+});
