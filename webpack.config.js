629d52e1e7f5f2e12fcaea8e18c2d55faf7d8b37
Dima Tsoy <dimktsoy@yahoo.com>
2020-07-16 11:45:08 +0600

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,7 @@ module.exports = {
         use: [
           {
             loader: 'pug-loader',
-            query: {
+            options: {
               pretty: true
             }
           }
